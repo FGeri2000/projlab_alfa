54104055104b0bb98285f7f257bd0ee74296e161
zsolszesz <zsolszesz@gmail.com>
2023-04-16 19:56:38 +0200

    Added pick up test functions

--- src/projlab/skeleton/PickupPlace.java
+++ src/projlab/skeleton/PickupPlace.java
@@ -96,7 +96,99 @@ public class PickupPlace {
         //Print calls to stdout
         CallHierarchyWriter.PrintToStandardOutput();
     }
-	//pick up pump
+    
+    public static void Test_PlumberPicksUpPumpFromPipe(){
+        CallHierarchyWriter.Clear();
+        System.out.println("Running use-case: Plumber Picks Up Pump From Pipe");
+
+        //Initialization
+        Pipe position = new Pipe();
+        Pump neighbor = new Pump();
+        Plumber pl = new Plumber(position);
+
+        ConditionQuerier.EnableDefaults();
+        position.AddNeighbor(neighbor);
+        position.PutPlayer(pl);
+        neighbor.AddNeighbor(position);
+        ConditionQuerier.DisableDefaults();
+
+        //Passing original caller (controller) to writer
+        String ctrl_dummy = "[controller]";
+        CallHierarchyWriter.PushCaller(ctrl_dummy);
+
+        CallHierarchyWriter.PushIdentifier(ctrl_dummy, ctrl_dummy);
+        CallHierarchyWriter.PushIdentifier(position, "position");
+        CallHierarchyWriter.PushIdentifier(pl, "pl");
+        CallHierarchyWriter.PushIdentifier(neighbor, "neighbor");
+
+        //Run simulation
+        pl.InputCallback_Pickup(ConditionQuerier.QueryUserForInteger("Hányadik szomszédot vegye fel a játékos?"));
+
+        //Print calls to stdout
+        CallHierarchyWriter.PrintToStandardOutput();
+    }
+    
+    public static void Test_PlumberPicksUpCisternFromPipe(){
+        CallHierarchyWriter.Clear();
+        System.out.println("Running use-case: Plumber Picks Up Cistern From Pipe");
+
+        //Initialization
+        Pipe position = new Pipe();
+        Cistern neighbor = new Cistern();
+        Plumber pl = new Plumber(position);
+
+        ConditionQuerier.EnableDefaults();
+        position.AddNeighbor(neighbor);
+        position.PutPlayer(pl);
+        neighbor.AddNeighbor(position);
+        ConditionQuerier.DisableDefaults();
+
+        //Passing original caller (controller) to writer
+        String ctrl_dummy = "[controller]";
+        CallHierarchyWriter.PushCaller(ctrl_dummy);
+
+        CallHierarchyWriter.PushIdentifier(ctrl_dummy, ctrl_dummy);
+        CallHierarchyWriter.PushIdentifier(position, "position");
+        CallHierarchyWriter.PushIdentifier(pl, "pl");
+        CallHierarchyWriter.PushIdentifier(neighbor, "neighbor");
+
+        //Run simulation
+        pl.InputCallback_Pickup(ConditionQuerier.QueryUserForInteger("Hányadik szomszédot vegye fel a játékos?"));
+
+        //Print calls to stdout
+        CallHierarchyWriter.PrintToStandardOutput();
+    }
+    
+    public static void Test_PlumberPicksUpSourceFromPipe(){
+        CallHierarchyWriter.Clear();
+        System.out.println("Running use-case: Plumber Picks Up Source From Pipe");
+
+        //Initialization
+        Pipe position = new Pipe();
+        Source neighbor = new Source();
+        Plumber pl = new Plumber(position);
+
+        ConditionQuerier.EnableDefaults();
+        position.AddNeighbor(neighbor);
+        position.PutPlayer(pl);
+        neighbor.AddNeighbor(position);
+        ConditionQuerier.DisableDefaults();
+
+        //Passing original caller (controller) to writer
+        String ctrl_dummy = "[controller]";
+        CallHierarchyWriter.PushCaller(ctrl_dummy);
+
+        CallHierarchyWriter.PushIdentifier(ctrl_dummy, ctrl_dummy);
+        CallHierarchyWriter.PushIdentifier(position, "position");
+        CallHierarchyWriter.PushIdentifier(pl, "pl");
+        CallHierarchyWriter.PushIdentifier(neighbor, "neighbor");
+
+        //Run simulation
+        pl.InputCallback_Pickup(ConditionQuerier.QueryUserForInteger("Hányadik szomszédot vegye fel a játékos?"));
+
+        //Print calls to stdout
+        CallHierarchyWriter.PrintToStandardOutput();
+    }
 	//place pipe from p/s/c
 	//place pump by splitting pipe
 	//place pump on end of pipe
