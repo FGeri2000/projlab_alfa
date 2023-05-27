1b7f3ed4f59c3f4f508808a0fda54a6bd9277cf4
zsolszesz <zsolszesz@gmail.com>
2023-05-27 19:54:15 +0200

    Added Move, Pickup and Place buttons

---
+++ src/projlab/graphics/PickupButton.java
@@ -0,0 +1,56 @@
+package projlab.graphics;
+
+import java.awt.event.*;
+
+import javax.swing.JButton;
+
+/**
+ * A játékosokon keresztüli csőhálózati elemek felvételéért felelős gomb osztálya.
+ */
+public class PickupButton extends JButton implements ActionListener {
+	/**
+     * A gombra klikkelve indított műveletek az itt hivatkozott
+     * játékost reprezentáló grafikus elemen végrehajtandók.
+     */
+	private PlayerGraphic targetObject;
+	
+	/**
+     * A gombot létrehozó konstruktor.
+     * @param targetObject Az a PlayerGraphic objektum, amin a műveletek elvégzendőek.
+     */
+	public PickupButton(PlayerGraphic targetObject)
+	{
+		super("Pick up");
+		this.targetObject = targetObject;
+		addActionListener(this);
+	}
+	/**
+     * Visszaadja a tárolt PlayerGraphic objektumot.
+     * @return targetObject A tárolt PlayerGraphic objektum.
+     */
+	public PlayerGraphic getTargetObject()
+	{
+		return targetObject;
+	}
+	/**
+     * Módosítja a tárolt PlayerGraphic objektumot a paraméterben megadottra
+     * @param targetObject Az új PlayerGraphic objektum.
+     */
+	public void setTargetObject(PlayerGraphic targetObject)
+	{
+		if (targetObject == null)
+			throw new IllegalArgumentException();
+		this.targetObject = targetObject;
+	}
+
+	/**
+     * Kezdeményezi a játékoson keresztül a csőhálózati objektum felvételét,
+     * ha a felhasználó a gombra kattint.
+     * @param e The event to be processed.
+     */
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		targetObject.pickupObject();
+	}
+}
