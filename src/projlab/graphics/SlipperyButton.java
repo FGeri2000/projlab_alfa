33dcb25803cc38e951db6b0b2d61eef6a3068670
szfp98 <pal.szabofeltothy@gmail.com>
2023-05-28 18:47:43 +0200

    PipeGraphic, StickyButton, SlipperyButton

---
+++ src/projlab/graphics/SlipperyButton.java
@@ -0,0 +1,50 @@
+package projlab.graphics;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+
+/**
+ * A csövet megjelenítő vízhálózati elemek csúszóssá tételéhez szükséges felhasználói
+ * interakcióért felelős gomb. Csak akkor aktív, ha a kiválasztott elem egy
+ * csövet reprezentál, ami nem csúszik.
+ */
+public class SlipperyButton extends JButton implements ActionListener {
+    /**
+     * A gombra klikkelve indított műveletek az itt hivatkozott
+     * csövet reprezentáló grafikus elemen végrehajtandók.
+     */
+    private PipeGraphic targetObject;
+    /**
+     * A gombot létrehozó konstruktor. A
+     * paraméter a targetObject attribútum, amin gombnyomásra a műveletek elvégzendők.
+     * @param targetObject
+     */
+    public SlipperyButton(PipeGraphic targetObject){
+        super();
+        this.targetObject = targetObject;
+        if(targetObject.getBindingObject().isSlippery()) setEnabled(false);
+    }
+
+    /**
+     * A JButton függvényének felülírása. Ha a
+     * paraméter bal egérgombbal kattintás által kiváltott esemény, a gomb meghívja a
+     * targetObjectben tárolt grafikus elemhez kapcsolt cső elem “turnSlippery” metódusát.
+     * @param e the event to be processed
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if((e.getModifiers() & InputEvent.BUTTON1_DOWN_MASK) != 0){
+            if(targetObject.getBindingObject().turnSlippery()){
+                setEnabled(false);
+            }
+        }
+    }
+    public PipeGraphic getTargetObject(){return targetObject;}
+    public void setTargetObject(PipeGraphic targetObject){
+        if(targetObject == null) throw new IllegalArgumentException();
+        this.targetObject = targetObject;
+        if(!targetObject.getBindingObject().isSlippery()) setEnabled(true);
+    }
+}
